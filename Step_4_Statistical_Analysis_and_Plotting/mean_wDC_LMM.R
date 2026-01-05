6fff3f4aceaaea2c61bbd75728540128fcd2981d
Mihail Dimitrov <15203653+misho-dimitrov@users.noreply.github.com>
2026-01-05 17:18:22 +0000

    Added code for FDR and DoF; rectified comments for subsetting

--- Step_4_Statistical_Analysis_and_Plotting/mean_wDC_LMM.R
+++ Step_4_Statistical_Analysis_and_Plotting/mean_wDC_LMM.R
@@ -1,8 +1,30 @@
 # Load required libraries
 library(ptestr)
+library(lmerTest)
+library(stats)
 library(readr)
 
 setwd('~/Downloads/PhD/Analysis/Tianeptine/Static_FC/Jupyter_test_notebook/Analysis/Mean_wDC/')
+
+### Baseline ###
+hyper_wDC_df <- read.csv("hyper_LMM.csv")
+hyper_wDC_df <- hyper_wDC_df[, -1]
+
+hyper_wDC_df_baseline <- subset(hyper_wDC_df, drug == 0)
+hyper_wDC_df_baseline <- hyper_wDC_df_baseline[, -which(names(hyper_wDC_df_baseline) == "drug")]
+hyper_wDC_df_baseline <- hyper_wDC_df_baseline[, -which(names(hyper_wDC_df_baseline) == "ID")]
+
+hyper_wDC_df_baseline.permuted <- grouped_perm_glm(hyper_wDC_df_baseline, formla = wDC ~ group + mFD, var_to_perm='wDC', permNum = 5000, seed = 42)
+
+###############
+hypo_wDC_df <- read.csv("hypo_LMM.csv")
+hypo_wDC_df <- hypo_wDC_df[, -1]
+
+hypo_wDC_df_baseline <- subset(hypo_wDC_df, drug == 0)
+hypo_wDC_df_baseline <- hypo_wDC_df_baseline[, -which(names(hypo_wDC_df_baseline) == "drug")]
+hypo_wDC_df_baseline <- hypo_wDC_df_baseline[, -which(names(hypo_wDC_df_baseline) == "ID")]
+
+hypo_wDC_df_baseline.permuted <- grouped_perm_glm(hypo_wDC_df_baseline, formla = wDC ~ group + mFD, var_to_perm='wDC', permNum = 5000, seed = 42)
 
 ########################################################
 
@@ -37,7 +59,7 @@ hyper_wDC_NT.permuted <- grouped_perm_glmm(hyper_wDC_df_NT, formla = wDC ~ drug 
 
 write_csv(hyper_wDC_NT.permuted, "hyper_NT_model_output.csv")
 ###############
-# Filter rows where group = 0
+# Filter rows where group = 1
 hyper_wDC_df_A <- subset(hyper_wDC_df, group == 1)
 
 # Remove the group column
@@ -63,7 +85,7 @@ hypo_wDC_NT.permuted <- grouped_perm_glmm(hypo_wDC_df_NT, formla = wDC ~ drug + 
 
 write_csv(hypo_wDC_NT.permuted, "hypo_NT_model_output.csv")
 ###############
-# Filter rows where group = 0
+# Filter rows where group = 1
 hypo_wDC_df_A <- subset(hypo_wDC_df, group == 1)
 
 # Remove the group column
@@ -75,3 +97,41 @@ hypo_wDC_df_A <- hypo_wDC_df_A[, -which(names(hypo_wDC_df_A) == "group")]
 hypo_wDC_A.permuted <- grouped_perm_glmm(hypo_wDC_df_A, formla = wDC ~ drug + mFD + (1|ID), var_to_perm='wDC', permNum = 5000, seed = 42)
 
 write_csv(hypo_wDC_A.permuted, "hypo_A_model_output.csv")
+
+########################################################
+### FDR correction ###
+corr_p_vals <- p.adjust(c(0.043, 0.099, 0.335, 0.017, 0.980, 0.872, 0.052, 0.002), method = "BY")
+corr_p_vals
+
+########################################################
+### DoF estimation ###
+
+## Interactions ##
+# Frontoparietal
+hyper_wDC.get_DoF <- lmer(
+  wDC ~ group * drug + mFD + (1|ID),
+  data = hyper_wDC_df
+)
+summary(hyper_wDC.get_DoF)
+
+# Sensorimotor
+hypo_wDC.get_DoF <- lmer(
+  wDC ~ group * drug + mFD + (1|ID),
+  data = hypo_wDC_df
+)
+summary(hypo_wDC.get_DoF)
+
+## Within-A ##
+# Frontoparietal
+hyper_wDC_A.get_DoF <- lmer(
+  wDC ~ drug + mFD + (1|ID),
+  data = hyper_wDC_df_A
+)
+summary(hyper_wDC_A.get_DoF)
+
+# Sensorimotor
+hypo_wDC_A.get_DoF <- lmer(
+  wDC ~ drug + mFD + (1|ID),
+  data = hypo_wDC_df_A
+)
+summary(hypo_wDC_A.get_DoF)
